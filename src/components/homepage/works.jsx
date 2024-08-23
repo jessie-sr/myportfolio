329f61821d370557e49469b05fe982cc6b340a5e
jessie-sr <jessie_sr@163.com>
2024-08-23 18:39:41 +0800

    Updated work

--- src/components/homepage/works.jsx
+++ src/components/homepage/works.jsx
@@ -10,9 +10,35 @@ const Works = () => {
 		<div className="works">
 			<Card
 				icon={faSchool}
-				title="Academic Background"
+				title="EDUCATION"
 				body={
 					<div className="works-body">
+                        <div className="work">
+							<img
+								src="./upenn.png"
+								alt="upenn"
+								className="work-image"
+							/>
+							<div className="work-title">University of Pennsylvania</div>
+							<div className="work-subtitle">
+                                M.S. Computer Science
+							</div>
+							<div className="work-duration">Aug 2024 - Present</div>
+						</div>
+
+                        <div className="work">
+							<img
+								src="./ucb.png"
+								alt="ucb"
+								className="work-image"
+							/>
+							<div className="work-title">University of California, Berkeley</div>
+							<div className="work-subtitle">
+								Visiting Student - CS Track
+							</div>
+							<div className="work-duration">Jan 2023 - Dec 2023</div>
+						</div>
+
 						<div className="work">
 							<img
 								src="./sufe.png"
@@ -21,22 +47,9 @@ const Works = () => {
 							/>
 							<div className="work-title">Shanghai University of Finance and Economics</div>
 							<div className="work-subtitle">
-								International Accounting (ACCA)
+                                B.S. Accounting (ACCA)
 							</div>
-							<div className="work-duration">Sep 2020 - Jul 2024</div>
-						</div>
-
-						<div className="work">
-							<img
-								src="./ucb.png"
-								alt="ucb"
-								className="work-image"
-							/>
-							<div className="work-title">University of California, Berkeley</div>
-							<div className="work-subtitle">
-								Visiting Student
-							</div>
-							<div className="work-duration">Jan 2023 - Dec 2023</div>
+							<div className="work-duration">Sep 2020 - Jun 2024</div>
 						</div>
 					</div>
 				}
@@ -44,39 +57,52 @@ const Works = () => {
 
 			<Card
 				icon={faBriefcase}
-				title="Professional Experiences"
+				title="EXPERIENCE"
 				body={
 					<div className="works-body">
+						<div className="work">
+							<img
+								src="./dashmote.png"
+								alt="dashmote"
+								className="work-image"
+							/>
+							<div className="work-title">Dashmote Netherlands, Inc</div>
+							<div className="work-subtitle">
+								Software Development Engineer Intern
+							</div>
+							<div className="work-duration">Mar 2024 - Aug 2024</div>
+						</div>
+
+						<div className="work">
+							<img
+								src="./swoop.png"
+								alt="swoop"
+								className="work-image"
+							/>
+							<div className="work-title">Swoop Studio</div>
+							<div className="work-subtitle">
+								Software Engineer
+							</div>
+							<div className="work-duration">Jan 2024 - Jun 2024</div>
+						</div>
+
 						<div className="work">
 							<img
 								src="./tencent.png"
 								alt="tencent"
 								className="work-image"
 							/>
-							<div className="work-title">Shenzhen Tencent Computer System Co. Ltd.</div>
+							<div className="work-title">Tencent</div>
 							<div className="work-subtitle">
-								Research Assistant
+								Data Analyst Intern
 							</div>
-							<div className="work-duration">Mar 2022 - Apr 2022</div>
-						</div>
-
-						<div className="work">
-							<img
-								src="./huaxin.png"
-								alt="huaxin"
-								className="work-image"
-							/>
-							<div className="work-title">China Fortune Securities</div>
-							<div className="work-subtitle">
-								Macroeconomic Research Intern
-							</div>
-							<div className="work-duration">Jun 2023 - Nov 2023</div>
+							<div className="work-duration">Apr 2022 - May 2022</div>
 						</div>
 					</div>
 				}
 			/>
 
-			<Card
+			{/* <Card
 				icon={faFaceGrin}
 				title="Campus Involvement"
 				body={
@@ -122,43 +148,7 @@ const Works = () => {
 						
 					</div>
 				}
-			/>
-
-			<Card
-				icon={faUserGroup}
-				title="Leadership Experiences"
-				body={
-					<div className="works-body">
-						<div className="work">
-							<img
-								src="./ziteng.png"
-								alt="ziteng"
-								className="work-image"
-							/>
-							<div className="work-title">Ziteng Street Neighborhood Committee</div>
-							<div className="work-subtitle">
-								Community Volunteer
-							</div>
-							<div className="work-duration">Apr 2022 - Jun 2022</div>
-						</div>
-
-						<div className="work">
-							<img
-								src="./qiancun.png"
-								alt="qiancun"
-								className="work-image"
-							/>
-							<div className="work-title">"Thousand Villages Survey" Project</div>
-							<div className="work-subtitle">
-								Team Leader
-							</div>
-							<div className="work-duration">Jul 2022 - Aug 2022</div>
-						</div>
-						
-					</div>
-				}
-			/>
-
+			/> */}
 		</div>
 	);
 };
